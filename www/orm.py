d072aa0b8119d3d48d618261b1ed001bb25e6946
wyqqyyww <wyqqyyww@163.com>
2017-07-23 14:32:24 +0800

    完成字段定义，表映射建立

--- www/orm.py
+++ www/orm.py
@@ -32,3 +32,28 @@ class Model(dict,metaclass=ModelMetaclass):
     def getValueOrDefault(self,key):
         pass
 
+class Field(object):
+    def __init__(self,name,column_type,primary_key,default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return "<%s,s%:s%>"%(self.__class__.__name__,self.column_type,self.name)
+
+class StringField(Field):
+    def __init__(self,name,ddl='varchar(100)',primary_key=False,default=None):
+        return super.__init__(name,ddl,primary_key,default)
+
+class FloatField(Field):
+    def __init__(self,name,ddl='float(20)',primary_key=False,default=None):
+        return super.__init__(name,ddl,primary_key,default)
+
+class TextField(Field):
+    def __init__(self,name,ddl='text(1000)',primary_key=False,default=None):
+        return super.__init__(name,ddl,primary_key,default)
+
+class BooleanField(Field):
+    def __init__(self,name,ddl='boolean',primary_key=False,default=True):
+        return super.__init__(name,ddl,primary_key,default)
